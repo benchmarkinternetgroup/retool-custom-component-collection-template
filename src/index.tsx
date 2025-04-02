9c92084760a652fadb62e821cc2f612f5e138dcd
Ronald Liang <2927782+maxr37@users.noreply.github.com>
2025-04-02 15:03:44 -0700

    unlayer editor function and style updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useState } from 'react'
+import './styles.css'
+import React, { useEffect, useRef, useState } from 'react'
 import EmailEditor, { EditorRef, EmailEditorProps } from 'react-email-editor'
 import { Retool } from '@tryretool/custom-component-support'
 import { scratch } from './email-templates/scratch'
-import { sample } from './email-templates/sample'
+
 export const UnlayerEditor = () => {
   const [projectId] = Retool.useStateString({ name: 'projectId' })
   const [emailDesign, setEmailDesign] = Retool.useStateString({
@@ -10,6 +11,7 @@ export const UnlayerEditor = () => {
   })
   const [currentDesign, setCurrentDesign] = useState('')
   const [emailHtml, setEmailHtml] = Retool.useStateString({ name: 'emailHtml' })
+  const triggerSave = Retool.useEventCallback({ name: 'triggerSave' })
   const emailEditorRef = useRef<EditorRef>(null)
   const isJson = (str: string) => {
     try {
@@ -32,30 +34,26 @@ export const UnlayerEditor = () => {
   }
 
   const onReady: EmailEditorProps['onReady'] = () => {
+    loadEmailDesignFromState()
+  }
+
+  const updateDesign = () => {
+    const unlayer = emailEditorRef.current?.editor
+    const parsedDesign =
+      currentDesign && isJson(currentDesign) ? JSON.parse(currentDesign) : null
+    if (parsedDesign) {
+      unlayer?.loadDesign(parsedDesign)
+    }
+  }
+
+  const loadEmailDesignFromState = () => {
+    const unlayer = emailEditorRef.current?.editor
     const parsedDesign =
       emailDesign && isJson(emailDesign)
         ? JSON.parse(emailDesign)
         : JSON.parse(scratch)
-    loadEmailDesign(parsedDesign)
+    unlayer?.loadDesign(parsedDesign)
     setCurrentDesign(JSON.stringify(parsedDesign))
-  }
-
-  const updateDesign = () => {
-    const parsedDesign =
-      currentDesign && isJson(currentDesign) ? JSON.parse(currentDesign) : null
-    if (parsedDesign) {
-      loadEmailDesign(parsedDesign)
-    }
-  }
-
-  const loadEmailDesign = (design: any) => {
-    const unlayer = emailEditorRef.current?.editor
-    unlayer?.loadDesign(design)
-  }
-
-  const loadSampleDesign = () => {
-    const unlayer = emailEditorRef.current?.editor
-    unlayer?.loadDesign(JSON.parse(sample))
   }
 
   const newDesign = () => {
@@ -63,51 +61,60 @@ export const UnlayerEditor = () => {
     unlayer?.loadDesign(JSON.parse(scratch))
   }
 
-  const triggerSave = Retool.useEventCallback({ name: 'triggerSave' })
+  useEffect(() => {
+    loadEmailDesignFromState()
+  }, [emailDesign])
+
   return (
     <div>
-      <div style={{ marginBottom: '20px' }}>
-        <button style={{ marginLeft: '10px' }} onClick={() => newDesign()}>
+      <div style={{ marginBottom: '16px' }}>
+        <button className="nxg-button" onClick={() => newDesign()}>
           Clear Design
         </button>
-        <button style={{ marginLeft: '10px' }} onClick={() => updateDesign()}>
-          Load Design (from State)
-        </button>
-        <button style={{ marginLeft: '10px' }} onClick={() => updateDesign()}>
-          Update Design (from Input)
+        <button
+          className="nxg-button"
+          style={{ marginLeft: '8px' }}
+          onClick={() => updateDesign()}
+        >
+          Update Design from Input
         </button>
         <button
-          style={{ marginLeft: '10px' }}
-          onClick={() => loadSampleDesign()}
+          className="nxg-button nxg-button--primary"
+          onClick={saveEmail}
+          style={{ float: 'right' }}
         >
-          Load Sample Design
-        </button>
-        <button onClick={saveEmail} style={{ float: 'right' }}>
           Save Email
         </button>
       </div>
 
       <EmailEditor
-        style={{ width: '100%', height: '800px', marginBottom: '20px' }}
+        style={{ width: '100%', height: '800px', marginBottom: '16px' }}
         ref={emailEditorRef}
         onReady={onReady}
-        projectId={projectId}
+        options={{
+          projectId: parseInt(projectId),
+          version: 'latest',
+          appearance: {
+            theme: 'modern_light',
+            panels: {
+              tools: {
+                dock: 'left'
+              }
+            }
+          }
+        }}
       />
-      <div style={{ marginBottom: '20px' }}>
-        <label>Email Design</label>
+      <div style={{ marginBottom: '16px' }}>
+        <label>Email Design (JSON)</label>
         <textarea
-          style={{ width: '100%', height: '100px' }}
+          className="nxg-textarea"
           value={currentDesign}
           onChange={(e) => setCurrentDesign(e.target.value)}
         />
       </div>
       <div>
         <label>Email HTML</label>
-        <textarea
-          style={{ width: '100%', height: '100px' }}
-          value={emailHtml}
-          disabled={true}
-        />
+        <textarea className="nxg-textarea" value={emailHtml} disabled={true} />
       </div>
     </div>
   )
